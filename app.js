5ec509a06dfff34b1f03c94f201a612a1504b602
Mozin Omer <mozinomer12@gmail.com>
2019-04-19 14:36:07 +0500

    5th_commit

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const function_from = require('./notes')
 const chalk = require('chalk')
-const write = function_from()
-console.log(write)
+// const write = function_from()
+// console.log(write)
 
-console.log(`
-CPU: ${chalk.red.bold('90%')}
-RAM: ${chalk.green('40%')}
-DISK: ${chalk.yellow('70%')}
-`);
+// console.log(`
+// CPU: ${chalk.blue.bold('90%')}
+// RAM: ${chalk.green('40%')}
+// DISK: ${chalk.yellow('70%')}
+// `);
+
+// console.log(process.argv[2])
+
+const command = process.argv[2]
+
+console.log(progress.argv)
+
+if (command === 'add') {
+  console.log('adding note')
+}
+else
+{
+  console.log('alisjdiasj')
+}
